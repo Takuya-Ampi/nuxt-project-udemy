92129b1112ebe4cc23432eb367f47cd83cb1801a
行比拓也 <imac@iMac.local>
2020-06-28 19:19:34 +0900

    エラー処理

--- pages/index.vue
+++ pages/index.vue
@@ -13,15 +13,20 @@
 
 <script>
 const axios = require("axios");
-let url = "https://jsonplaceholder.typicode.com/users";
+let url = "https://jsonplaceholder.typicode.com/usersxxx";
 
 export default {
-  asyncData({ params }) {
+  asyncData({ params, error }) {
     return axios.get(url)
       .then((res) => {
         // users:はマスタッシュ構文で表示するために設定
         return { users: res.data }
       })
+      .catch((e => {
+        // console.log(e.response.status);
+        // error({users: e.response.status, message: e.message})
+        error({users: e.response.status, message: 'error!!!!!'})
+      }))
   }
 };
 </script>
