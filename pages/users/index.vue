e1aec6963eb7f4a50cc1ae64f2ac84269ab9b54e
行比拓也 <imac@iMac.local>
2020-06-28 13:54:11 +0900

    階層構造のルーティング

---
+++ pages/users/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="container">
+    <p>{{ message }}</p>
+    <!-- <router-link to="u/price">Price Page</router-link> -->
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "hello user"
+    };
+  }
+};
+</script>
+
+<style>
+</style>
