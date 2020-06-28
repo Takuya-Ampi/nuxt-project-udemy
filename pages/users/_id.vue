0335a5801d908a655a7bba6070a4bb5810578e78
行比拓也 <imac@iMac.local>
2020-06-28 17:00:53 +0900

    動的なルーティング。this..params.idによるパラメーターの受け渡し

---
+++ pages/users/_id.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="container">
+    <p>{{ message }}</p>
+    <p>id: {{ this.$route.params.id }}</p>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "/users/_id.vue"
+    };
+  }
+};
+</script>
+
+<style>
+</style>
