120353c9704069f2582195215ebb96ea01bb8b7b
行比拓也 <imac@iMac.local>
2020-06-28 17:07:54 +0900

    パラメータのバリデーション

--- pages/users/_id.vue
+++ pages/users/_id.vue
@@ -11,6 +11,13 @@ export default {
     return {
       message: "/users/_id.vue"
     };
+  },
+  // バリデーションに関しては,Nuxt公式に書いてある
+  // 参考 https://ja.nuxtjs.org/api/pages-validate/
+  // /^\d+$/は正規表現
+  // 数値でなければならない
+  validate({ params }){
+    return /^\d+$/.test(params.id)
   }
 };
 </script>
