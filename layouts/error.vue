dbc2034c4dd887a8012ea0b076fe9d9ba33aacbc
行比拓也 <imac@iMac.local>
2020-06-28 17:31:29 +0900

    エラーページのカスタマイズ

---
+++ layouts/error.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="container">
+    <!-- v-ifはなくても良い -->
+    <p v-if="error.statusCode === 404">エラー 404</p>
+  </section>
+</template>
+
+<script>
+export default {
+  // 下記記述によりステータスコードを受け取ることができるようになる
+  props: ['error']
+};
+</script>
+
+<style>
+</style>
